b2465af55e5e3e543fee869cac34eb40ebd92764
CaioAssis <132146686+CaioAssis@users.noreply.github.com>
2025-07-19 14:42:55 -0300

    adiciona sbox, separa caracteres hex
    Co-Authored-By: wesleySenciani <105518482+WSenci@users.noreply.github.com>

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -8,42 +8,57 @@ public class Main {
 		System.out.println("Hello World");
 
 		String texto = "Eu sou um cara legal, voce nao acha?";
-
-		// Passo 1: transformar a String em bytes
+		
 		byte[] bytes = texto.getBytes(StandardCharsets.UTF_8);
-
-		// Passo 2: transformar os bytes em uma string hexadecimal
 		StringBuilder hexString = new StringBuilder();
 		for (byte b : bytes) {
-			// "%02x" garante que sempre teremos dois dígitos (ex: '0a' ao invés de 'a')
 			hexString.append(String.format("%02x", b));
 		}
-		byte[][][] matriz = create_matrix(bytes);
-		//System.out.printf("%02x", matriz[0][0]);
+//		byte[][][] matriz = create_matrix(bytes);
+//		show_matrix(matriz);
+
+		//System.out.printf("%02x", (byte)' ');
+
+//		//separacao coordenadas hexa para s-box
+//		int y = (byte)'j'/16;
+//		int z = Math.floorMod((byte)'j',16);
+//		
+//		System.out.printf("%01d\n",y);
+//		System.out.printf("%01d", z);
+
+		//byte[][] uau = SBox.get_sbox();
+		//SBox.show_sbox(uau);
 	}
 
 	public static byte[][][] create_matrix(byte[] entrada) {
 		System.out.println("Entrou");
 		int pointer = 0;
-		int quantidade = (entrada.length / 16)+1;
+		int quantidade = (entrada.length / 16) + 1;
 		byte[][][] matriz = new byte[quantidade][4][4];
 		for (int h = 0; h < quantidade; h++) {
 			for (int i = 0; i < 4; i++) {
 				for (int j = 0; j < 4; j++) {
 					if (entrada.length > pointer) {
-						matriz[h][j][i] = entrada[pointer];
-						System.out.printf("[%02x] ", matriz[h][j][i]);
-					}
-					else {
-						matriz[h][j][i] = (byte)' ';
-						System.out.printf("[%02x] ", matriz[h][j][i]);
+						matriz[h][i][j] = entrada[pointer];
+					} else {
+						matriz[h][i][j] = (byte) ' ';
 					}
 					pointer++;
 				}
-				System.out.println("");
 			}
-			System.out.println("fim");
 		}
 		return matriz;
+	}
+
+	public static void show_matrix(byte[][][] matriz) {
+		for (int h = 0; h < matriz.length; h++) {
+			for (int i = 0; i < matriz[h].length; i++) {
+				for (int j = 0; j < matriz[h][i].length; j++) {
+					System.out.printf("[%02x] ", matriz[h][i][j]);
+				}
+				System.out.println("");
+			}
+			System.out.println("\n");
+		}
 	}
 }
