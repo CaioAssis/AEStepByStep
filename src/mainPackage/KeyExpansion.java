463fbd00a6767f5a2bdfd1e117d86ceae1884ebf
CaioAssis <132146686+CaioAssis@users.noreply.github.com>
2025-07-24 19:31:28 -0300

    cria rcon
    Co-Authored-By: wesleySenciani <105518482+WSenci@users.noreply.github.com>

--- src/mainPackage/KeyExpansion.java
+++ src/mainPackage/KeyExpansion.java
@@ -20,17 +20,24 @@ public class KeyExpansion {
 			y = word[i] % 16;
 			saida[i] = sbox[x][y];
 		}
-		
 		return saida;
 	}
 	
-	
-	
-	public static void main(String[] args) {
-		byte[] in = {(byte)0x00, (byte)0x02, (byte)0x03, (byte)0x04, };
-		//in = rot_word(in);
-		in = sub_word(in);
-		System.out.printf("%02x", in[0]);
+	public static byte[] rcon(byte[] word, int round){
+		byte[] saida = new byte[4];
+		byte[] rcon = Rcon.get_round_rcon(round);
+		
+		for(int i=0; i<4; i++) {
+			saida[i] = (byte)(word[i] ^ rcon[i]);
+		}
+		return saida;
 	}
 	
+	public static void main(String[] args) {
+		byte[] in = {(byte)0x01, (byte)0x02, (byte)0x03, (byte)0x04, };
+		//in = rot_word(in);
+		//in = sub_word(in);
+		in = rcon(in, 8);
+		System.out.printf("%02x", in[0]);
+	}
 }
