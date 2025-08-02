e27f1de3c6826e86093ff82dfc184796e94783b0
CaioAssis <132146686+CaioAssis@users.noreply.github.com>
2025-08-02 13:32:56 -0300

    altera args label
    Co-Authored-By: wesleySenciani <105518482+WSenci@users.noreply.github.com>

--- src/interfaces/Components.java
+++ src/interfaces/Components.java
@@ -5,24 +5,38 @@ import javafx.scene.control.TextArea;
 
 public class Components {
 
-	public TextArea textArea() {
+	public TextArea textArea(String placeholder) {
 		TextArea txtarea = new TextArea();
 		txtarea.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-		txtarea.setPromptText("texto aqui");
-		//txtarea.textProperty().addListener((obs, aval, val) -> System.out.println(val));
+		txtarea.setPromptText(placeholder);
 		txtarea.setWrapText(true);
 		txtarea.getStyleClass().add("text-area");
-		
+
+		// txtarea.textProperty().addListener((obs, aval, val) ->
+		// System.out.println(val));
+
 		return txtarea;
 	}
-	
-	public Label label(String text) {
+
+	public Label label(String text, StylesEnum style) {
+		String selected_style = "label-body";
+		switch (style) {
+		case TITLE:
+			selected_style = "label-title";
+		case SUBTITLE:
+			selected_style = "label-subtitle";
+		case BODY:
+			selected_style = "label-body";
+		case ANNOTATION:
+			selected_style = "label-annotation";
+		}
+
 		Label label = new Label();
-		
+
 		label.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-		label.getStyleClass().add("label-annotation");
+		label.getStyleClass().add(selected_style);
 		label.setText(text);
-		
+
 		return label;
 	}
 }
